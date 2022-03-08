1b440a0e88aa96b6278264b3d6d2efbbd024c374
Ganeshreddy0712 <91110203+Ganeshreddy0712@users.noreply.github.com>
2022-03-08 16:41:52 +0530

    Create Greatestamong3.py

---
+++ Greatestamong3.py
@@ -0,0 +1,9 @@
+a=int(input("Enter first number : "))
+b=int(input("Enter second number : "))
+c=int(input("Enter third number : "))
+if a>b and a>c :
+  print("A is the greatest among the given 3 : ",a)
+elif b>a and b>c :  
+    print("B is the greatest among the given 3 : ",b)
+else :
+    print("C is the greatest among the given 3 : ",c)
