b09e78045c70dbc8fb13f5c79ec08a48b8178a03
Ganeshreddy0712 <91110203+Ganeshreddy0712@users.noreply.github.com>
2022-03-08 15:49:02 +0530

    Update and rename README.md to ArthOperations.py

---
+++ ArthOperations.py
@@ -0,0 +1,13 @@
+# Pythonex-01
+
+a=int(input("Enter first number : "))
+b=int(input("Enter second number : "))
+c=int(input("Enter third number : "))
+sum=a+b+c
+diff=a-b-c
+product=a*b*c
+div=a/b/c
+print("Sum of given 3 numbers is : ",sum)
+print("Difference of given 3 numbers is : ",diff)
+print("Product of given 3 numbers is : ",product)
+print("Divison of given 3 numbers is : ",div)
